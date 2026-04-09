9e9799f764757546d5daa011dda88d10c03b2434
Leonardo <leoprietoc@gmail.com>
2026-04-09 00:26:02 -0500

    fix: storageBucket correcto firebasestorage.app

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDU11FxZaSGlnos4fUx8o9UffHBn-WArqg",
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "inom-recetas.firebaseapp.com",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "inom-recetas",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "inom-recetas.appspot.com",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "inom-recetas.firebasestorage.app",
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "631742948478",
   appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:631742948478:web:e1b3606bcfeede25f4242f",
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-L19NTSVWJE"
